977c7f2f378c79d8b85923bc3edf36c9db6ba974
caretot <117480141+caretot@users.noreply.github.com>
2024-05-24 19:57:27 +0400

    Add files via upload

--- rankometer/tablesearch.js
+++ rankometer/tablesearch.js
@@ -1,22 +1,23 @@
 function myFunctiona() {
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[3];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }       
-  }
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        tdID = tr[i].getElementsByTagName("td")[1];
+        tdName = tr[i].getElementsByTagName("td")[3];
+        if (tdID || tdName) {
+            txtValueID = tdID.textContent || tdID.innerText;
+            txtValueName = tdName.textContent || tdName.innerText;
+            if (txtValueID.toUpperCase().indexOf(filter) > -1 || txtValueName.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }       
+    }
 }
-
 
 
 
